7c16800b54ce65e6f52f731550b3f3eae26db6a3
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2023-01-09 19:10:09 -0300

    Update exercicio3.js

--- 08 - avaliacao/exercicio3.js
+++ 08 - avaliacao/exercicio3.js
@@ -2,10 +2,10 @@
 // 3) Faça um programa que calcule e imprima o salário salário a ser transferido para um funcionário.
 // Para realizar o calculo receba o valor bruto do salário e o adicional dos benefícios.
 // O salário salário a ser transferido é calculado da seguinte maneira: 
-   
+
 //    valor bruto do salário - percentual de imposto mediante a faixa salarial + adicional dos benefícios
 
-// Para calcular o percentual de imposto segue as aliquotas:
+// Para calcular o percentual de imposto segue as alíquotas:
 
 //     De R$ 0.00 a R$ 1100.00 = 5.00%
 //     De R$ 1100.01 a R$ 2500.00 = 10.00%
@@ -20,3 +20,16 @@
 //             2050.00
 
 const { gets, print } = require('./funcoes-auxiliares-ex3');
+
+const salarioBruto = gets();
+const benefios = gets();
+
+function calcularPorcentagem (valor, percentual) {
+    return valor *(percentual/100)
+}
+
+function pegarPercentualComBaseNoSalario(salario) {
+    
+}
+
+print(calcularPorcentagem(salarioBruto, 10));
