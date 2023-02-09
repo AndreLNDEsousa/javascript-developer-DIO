dae55531aff673f3d9c44a6c0a5f3190ce094b2a
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2023-02-09 18:46:41 -0300

    details

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,15 +1,17 @@
 
+function convertPokemonTypesToLi(pokemonTypes) {
+   return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>` )
+}
 
 function convertPokemonToLi(pokemon) {
    return `
     <li  class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">#${pokemon.order} </span>
     <span class="name"> ${pokemon.name} </span>
 
     <div class="detail">
        <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
+          ${convertPokemonTypesToLi(pokemon.types).join('')}
        </ol>
        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name} ">
        
@@ -21,7 +23,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons) => {
+pokeApi.getPokemons().then((pokemons = []) => {
 const newHtml = pokemons.map(convertPokemonToLi).join('')
 pokemonList.innerHTML += newHtml
 })
