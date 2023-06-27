5ac818aa8f17cf8cfbd0a78c5a864b52ae8a3aaa
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2023-06-27 03:11:40 -0300

    variáveis

---
+++ 09 - conceitos avancados/variaveis.js
@@ -0,0 +1,46 @@
+
+/*exer 1
+var1 = 10;
+var2 = 'teste';
+console.log(var1 + var2);
+*/
+
+/*exer 2
+teste();
+teste2();
+
+var teste1 = function () {
+    console.log('teste');
+}
+
+function teste2() {
+    console.log('teste2');
+}
+*/
+
+/* exer 3
+
+if (true) {
+    // var var1 = 10;
+    let var1 = 10; //não sofre hoisting, não vaza o valor para fora do bloco
+}
+console.log(var1);
+*/
+
+/* exer 4
+var _teste = 10;
+const TESTE - 10 // constante é declarada tudo em maiúsculo
+*/
+
+/* exer 05 
+var var1 = 10;
+var var2 = '20';
+
+console.log(var1 - var2);
+//saida -10, o javascript faz a conversão do texto para numero no caso de subtração
+*/
+/* exer 06
+*/
+//hoisting é o içamento da declaração para cima do código;
+
+
